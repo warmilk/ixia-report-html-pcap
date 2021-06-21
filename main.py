bf8d9364efbe9ab00376f32f72f5a365dd84a7b0
qianglong <qianglong@404.com>
2021-06-21 11:29:29 +0800

    改了几个html的空节点问题

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import requests
 读取ixiaBreakPointRrport.html并处理提取出有用的漏洞信息
 """
 def go():
-    response = urllib.request.urlopen('file:///D:/quying-work/pythonProject/assets/1.html', timeout=1)
+    response = urllib.request.urlopen('file:///D:/quying-work/pythonProject/assets/6.html', timeout=1)
     html = response.read()
     soup = BeautifulSoup(html, "lxml")
     # 获取html里面所有根据time of strike手动添加了class="strike-table"的table标签,从0开始计数，第12个之后的table才是有用的
@@ -28,9 +28,22 @@ def go():
             tr_list = tbody.find_all('tr')
             for tr in tr_list:
                 strike_name = tr.select_one("td:nth-of-type(2)").find('a').text
-                #将原始的html里面strike name自带的\n空格\t换行过滤走
-                result = re.sub(r'\s', '', strike_name)
-                print(result)
+                # 将原始的html里面strike name自带的\n空格\t换行过滤走
+                strike_name = re.sub(r'\s', '', strike_name)
+                # print(type(tr.select_one("td:nth-of-type(4)>div")))
+                # 判断td:nth-of-type(4)>div是否存在子节点
+                # td_div = tr.select_one("td:nth-of-type(4)>div")
+                # if type(td_div) == "<class 'NoneType'>":
+                #     strike_CVE = 'none'
+                #     print(strike_CVE)
+                # else:
+                #     strike_CVE = td_div.select_one("a").contents
+                #     print(strike_CVE)
+                print(tr.select_one("td:nth-of-type(4)>div>a").contents)
+
+
+
+
 
 
 if __name__ == '__main__':
